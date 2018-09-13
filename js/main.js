290de198193f81f0584473b0e85ab1b1ce4c8bcd
Scott Ammon <scott.o.ammon@gmail.com>
2018-09-13 10:13:40 -0700

    updated ajax request to include api key in header, reduced data output to table

--- js/main.js
+++ js/main.js
@@ -1,15 +1,34 @@
 $(document).ready(function() {
 
-    let url = 'https://finances.worldbank.org/resource/45tv-a6qy.json?$limit=10';
+    let limit = 5;
+    let url = 'https://finances.worldbank.org/resource/45tv-a6qy.json?$limit=' + limit;
 
-    $.get(url).done(function(data) {
+    $.ajax({
+      type: 'GET',
+      url: url,
+      beforeSend: function(xhr) {xhr.setRequestHeader('X-App-Token', 'oI0EZnx5rD82yqWmwIIO5BkpB')},
+      error: function(err) {console.log(err)}
+    }).done(function(data) {
       console.log(data);
+
+      // array of the desired columns in the table
+      let tableColumns = ['project_name', 
+                         'project_id',
+                         'major_sector',
+                         'procurement_category',
+                         'supplier',
+                         'supplier_country',
+                         'borrower_country',
+                         'supplier_contract_amount_usd',
+                         'wb_contract_number',
+                         'contract_signing_date'
+                        ];
       
       data.forEach(contract => {
         let tableRow = "";
-        for (let field in contract) {
-          tableRow = tableRow + ("<td>" + contract[field] + "</td>");
-        }
+        tableColumns.forEach(columnHeader => {
+          tableRow = tableRow + ("<td>" + contract[columnHeader] + "</td>");
+        });
         $('.table').append("<tr>" + tableRow + "</tr>");
       });
     });
